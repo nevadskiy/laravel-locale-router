c780d3ff35babb44fd0f24dcbbf81eca5242694a
Vitalik <nevadskiy@gmail.com>
2019-10-22 13:23:24 +0300

    Move url generator default logic

--- src/Listeners/LocaleUpdatedListener.php
+++ src/Listeners/LocaleUpdatedListener.php
@@ -2,24 +2,32 @@
 
 namespace Nevadskiy\LocalizationRouter\Listeners;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Foundation\Events\LocaleUpdated;
-use Nevadskiy\LocalizationRouter\Repositories\Repository;
+use Nevadskiy\LocalizationRouter\Repositories\UserLocaleRepository;
 
 class LocaleUpdatedListener
 {
     /**
-     * @var Repository
+     * @var UserLocaleRepository
      */
     private $repository;
 
     /**
+     * @var UrlGenerator
+     */
+    private $generator;
+
+    /**
      * LocaleUpdatedListener constructor.
      *
-     * @param Repository $repository
+     * @param UserLocaleRepository $repository
+     * @param UrlGenerator $generator
      */
-    public function __construct(Repository $repository)
+    public function __construct(UserLocaleRepository $repository, UrlGenerator $generator)
     {
         $this->repository = $repository;
+        $this->generator = $generator;
     }
 
     /**
@@ -31,5 +39,17 @@ class LocaleUpdatedListener
     public function handle(LocaleUpdated $event): void
     {
         $this->repository->set($event->locale);
+        $this->setDefaultUrlLocale($event->locale);
+    }
+
+    /**
+     * Set default URL locale to allow to generate all routes without passing a locale.
+     * E.g. Instead of 'route('articles', ['locale' => app()->getLocale()])' now available just 'route('article')'.
+     *
+     * @param string $locale
+     */
+    private function setDefaultUrlLocale(string $locale): void
+    {
+        $this->generator->defaults(['locale' => $locale]);
     }
 }
