b458289bab9a5b77ee5fa8ff3d610a6d4031996e
nevadskiy <nevadskiy@gmail.com>
2020-02-04 14:31:45 +0200

    Split into two separate listeners

---
+++ src/Listeners/RememberLocale.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Nevadskiy\LocalizationRouter\Listeners;
+
+use Illuminate\Foundation\Events\LocaleUpdated;
+use Nevadskiy\LocalizationRouter\Repositories\UserLocaleRepository;
+
+class RememberLocale
+{
+    /**
+     * @var UserLocaleRepository
+     */
+    private $repository;
+
+    /**
+     * RememberLocale constructor.
+     *
+     * @param UserLocaleRepository $repository
+     */
+    public function __construct(UserLocaleRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param LocaleUpdated $event
+     * @return void
+     */
+    public function handle(LocaleUpdated $event): void
+    {
+        $this->repository->set($event->locale);
+    }
+}
---
+++ src/Listeners/SwitchRouterLocale.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Nevadskiy\LocalizationRouter\Listeners;
+
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Foundation\Events\LocaleUpdated;
+
+class SwitchRouterLocale
+{
+    /**
+     * @var UrlGenerator
+     */
+    private $generator;
+
+    /**
+     * SwitchLocale constructor.
+     *
+     * @param UrlGenerator $generator
+     */
+    public function __construct(UrlGenerator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param LocaleUpdated $event
+     * @return void
+     */
+    public function handle(LocaleUpdated $event): void
+    {
+        $this->generator->defaults(['locale' => $event->locale]);
+    }
+}
